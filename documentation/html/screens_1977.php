5d459162b91755490da886d7ad86761a23483e66
Jason Rohrer <jasonrohrer@fastmail.fm>
2010-12-14 16:11:07 -0700

    Added secret screen shot page.

--- documentation/html/screens_1977.php
+++ documentation/html/screens_1977.php
@@ -1,19 +1,35 @@
+<html>
+
+<head>
+<title>Inside a Star-filled Sky</title>
+</head>
+
+<body bgcolor="#333333" text="#FFFFFF"
+      link="#FD8109" alink="#ffff00" vlink="#FD8109">
+
+<center>
+
+
+
 <?php
 
-// no caching
-header('Cache-Control: no-store, no-cache, must-revalidate');
-header('Cache-Control: post-check=0, pre-check=0', FALSE);
-header('Pragma: no-cache');
+$screenFiles = glob( "screens_1977/screen*.png" );
 
-error_reporting( E_ALL );
-
-
-include( "header.php" );
-
-
-
+foreach( $screenFiles as $f ) {
+    echo "<img width=\"640\" height=\"480\" src=\"$f\"><br><br><br><br>\n";
+    }
 ?>
 
+
+
+<!-- Site Meter -->
+<script type="text/javascript" src="http://s47.sitemeter.com/js/counter.js?site=s47insideastarfilledskymain">
+</script>
+<noscript>
+<a href="http://s47.sitemeter.com/stats.asp?site=s47insideastarfilledskymain" target="_top">
+<img src="http://s47.sitemeter.com/meter.asp?site=s47insideastarfilledskymain" alt="Site Meter" border="0"/></a>
+</noscript>
+<!-- Copyright (c)2009 Site Meter -->
 
 
 <?php include( "footer.php" ); ?>
