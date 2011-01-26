069f5af941f0ecc36925ee74b8b61d25e071aad1
Jason Rohrer <jasonrohrer@fastmail.fm>
2011-01-26 08:19:12 -0700

    Added double testing program.

---
+++ gameSource/testDouble.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {
+    
+    double x = (double)1 / (double)3;
+    
+    double yD = x * 3;
+
+    int yI = (int)(  yD );
+
+    FILE *outFile = fopen( "testDouble.txt", "w" );
+    
+    fprintf( outFile, 
+             "x = %f, (yD = x*3) = %f,  (int)yD = %d, (int)( x * 3 ) = %d\n", 
+             x, yD, (int)yD, (int)( x * 3 ) );
+    fclose( outFile );
+
+    return 0;
+    }
