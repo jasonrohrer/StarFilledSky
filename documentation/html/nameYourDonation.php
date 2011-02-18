71760905201ee0971a89382f22bc17a011fca0cc
Jason Rohrer <jasonrohrer@fastmail.fm>
2011-02-18 10:47:26 -0700

    Fixed extra-penny bug by renaming donation amount tag to be different from order tag.

--- documentation/html/nameYourDonation.php
+++ documentation/html/nameYourDonation.php
@@ -7,18 +7,21 @@ if( isset( $_REQUEST[ "donation" ] ) ) {
 
 $donationInCents = floor( 100 * $donation );
 
-if( $donationInCents > 1 ) {
-    // fix for FastSpring bug where it adds a penny to all custom prices
-    $donationInCents -= 1;
-    }
-else if( $donationInCents < 1 ) {
-    // FastSpring tags must be >= 1
-    $donationInCents = 1;
+
+
+if( $donationInCents < 0 ) {    
+    $donationInCents = 0;
     }
 
+// FastSpring tags must be >= 1
+// add 1 here, and subtract it later in FastSpring script
+
+$donationInCents += 1;
 
 
-header( "Location:https://sites.fastspring.com/jasonrohrer/instant/starfilledskyticket?tags=name_your_donation=$donationInCents")
+
+header( "Location:https://sites.fastspring.com/jasonrohrer/instant/starfilledskyticket?tags=donation_amount=$donationInCents");
+
 
 
 ?>
