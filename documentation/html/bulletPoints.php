6ee8aeece19cab737a5cd6db8148571ad7676120
Jason Rohrer <jasonrohrer@fastmail.fm>
2011-02-18 10:28:27 -0700

    More improved wording.

--- documentation/html/bulletPoints.php
+++ documentation/html/bulletPoints.php
@@ -22,31 +22,46 @@ include( "header.php" );
 <ul>
 
 
+<li>Your screen will be black with bullets.</li>
+<br>
+
 <li>Enter things.</li>
+<br>
+
 
 <li>Procedurally generated everything.  Almost everything.</li>
+<br>
+
 
 <li>100% boss-free.</li>
+<br>
+
 
 <li>This game would not exist without the inspiration that I drew from  games like Spelunky, Shoot First, Disgaea, Borderlands, Everyday Shooter, Psychonauts, Soul Reaver, and Prey.</li>
+<br>
 
-<li>"Larger than the known universe" is not an exaggeration.  You will reach the relative scale of the known universe after level 14.
+
+<li>"Larger than the known universe" is not an exaggeration.  You will reach the relative scale of the known universe after level 14.</li>
+<br>
+
 
 <li>165,000 basic weapon combinations is a lot.</li>
+<br>
 
-<li>Going down is much easier than going up.</li>
-
-<li>Your screen will be black with bullets.</li>
 
 <li>"Infinite" is kind of a misnomer, because 32-bit integers run out eventually.  You will reach the upper kill screen (one step up from level 2,147,483,647) after about 2043 years of continuous play.  Though with diligence, you can reach the lower kill screen in just 68 years.</li>
+<br>
+
 
 <li>53 people tested this game, and they played it an average of 10.89 times each.</li>
+<br>
+
 
 <li>This game is a tactical-situation generating machine.</li>
+<br>
+
 
 <li>That little guy at the top of the page is cute, but I'll never see him again.</li>
-
-<li>Grab a screenshot at any time by pressing the <b>=</b> key.</li>
 
 
 </ul>
