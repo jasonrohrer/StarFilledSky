a7dbd3db2aa94dd2ce175b9d1c8e96b3a380f5c8
Jason Rohrer <jasonrohrer@fastmail.fm>
2011-02-18 10:20:07 -0700

    Improved wording.

--- documentation/html/bulletPoints.php
+++ documentation/html/bulletPoints.php
@@ -30,7 +30,7 @@ include( "header.php" );
 
 <li>This game would not exist without the inspiration that I drew from  games like Spelunky, Shoot First, Disgaea, Borderlands, Everyday Shooter, Psychonauts, Soul Reaver, and Prey.</li>
 
-<li>"Larger than the known universe" is not an exaggeration.  I estimate that you will reach the relative scale of the known universe after about level 14.
+<li>"Larger than the known universe" is not an exaggeration.  You will reach the relative scale of the known universe after level 14.
 
 <li>165,000 basic weapon combinations is a lot.</li>
 
@@ -38,7 +38,7 @@ include( "header.php" );
 
 <li>Your screen will be black with bullets.</li>
 
-<li>"Infinite" is kind of a misnomer, because 32-bit integers run out eventually.  I estimate that you will reach the upper kill screen (one step up from level 2,147,483,647) after about 2043 years of continuous play.  Though with diligence, you can reach the lower kill screen in just 68 years.</li>
+<li>"Infinite" is kind of a misnomer, because 32-bit integers run out eventually.  You will reach the upper kill screen (one step up from level 2,147,483,647) after about 2043 years of continuous play.  Though with diligence, you can reach the lower kill screen in just 68 years.</li>
 
 <li>53 people tested this game, and they played it an average of 10.89 times each.</li>
 
